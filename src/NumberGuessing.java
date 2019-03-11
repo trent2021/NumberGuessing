c96f89859689f3842c1ef39bc15934f1d27b8272
Trent <trent.polenchar.2021@student.lhschools.org>
2019-03-11 13:55:55 -0400

    Created Intellij project

---
+++ src/NumberGuessing.java
@@ -0,0 +1,14 @@
+public class NumberGuessing {
+
+    public static void main(String[]args){
+
+        int pickedNumber;
+
+        pickedNumber = (int) (Math.random()*100);
+
+        System.out.println(pickedNumber);
+
+
+
+    }
+}
