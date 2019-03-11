2d39b3fbc01884bd00925ed63d913f78c7824304
Trent <trent.polenchar.2021@student.lhschools.org>
2019-03-11 14:18:13 -0400

    Started project

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,12 +1,46 @@
+import java.util.Scanner;
+
 public class NumberGuessing {
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
+
+        Scanner keyboard;
+        int userNumber;
+
+        keyboard = new Scanner(System.in);
+
 
         int pickedNumber;
 
-        pickedNumber = (int) (Math.random()*100);
+        pickedNumber = (int) (Math.random() * 100);
 
+
+        System.out.println("The random number is");
         System.out.println(pickedNumber);
+
+        System.out.println("Type a number from 1 to 100.");
+        userNumber = keyboard.nextInt();
+
+        if (userNumber < 1) {
+            System.out.println("That number is not from 1 to 100.");
+        }
+        else if (userNumber > 100) {
+            System.out.println("That number is not from 1 to 100.");
+        }
+        else if (userNumber > pickedNumber) {
+            System.out.println("You picked a higher number.");
+        }
+        else if (userNumber < pickedNumber) {
+            System.out.println("You picked a lower number.");
+        }
+
+
+
+
+
+
+
+
 
 
 
